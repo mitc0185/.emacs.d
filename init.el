07a76a9b794a8690815c1d3c28722b667e57ac95
Erik Mitchell <erik.x.mitchell@healthpartners.com>
2013-09-16 17:19:47 -0500

    Specific for Emacs.app to make it a little more svelte

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
 
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 ;; act like we've been here before
 (setq inhibit-startup-message t)
 
