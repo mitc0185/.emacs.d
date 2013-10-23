1f8f4ceeb8cdc41c2a1b8c193f5e893fd7c9dab7
Erik Mitchell <erik.mitchell@gmail.com>
2013-10-23 00:43:30 -0500

    Using occultation mode for java

--- mode-mappings.el
+++ mode-mappings.el
@@ -44,5 +44,8 @@
 (autoload 'markdown-mode "markdown-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
+;; Java
+(autoload 'occultation-mode "occultation-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.java$" . occultation-mode))
 
 (provide 'mode-mappings)
