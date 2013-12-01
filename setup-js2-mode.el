4d5fb8ab69dabf24c03525250c5216256cf495c2
Erik Mitchell <erikm@prototype-eq.com>
2013-11-30 22:03:19 -0600

    disabling slime mode for now and fixing spaces and tabs issue for js files

--- setup-js2-mode.el
+++ setup-js2-mode.el
@@ -1,7 +1,11 @@
 
-(global-set-key [f5] 'slime-js-reload)
+; (global-set-key [f5] 'slime-js-reload)
+;(add-hook 'js2-mode-hook
+;           (lambda ()
+;            (slime-js-minor-mode 1)))
+
 (add-hook 'js2-mode-hook
-          (lambda ()
-            (slime-js-minor-mode 1)))
+	  (lambda ()
+	    (setq indent-tabs-mode nil)))
 
 (provide 'setup-js2-mode)
