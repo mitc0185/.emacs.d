a33f5b9113eef3a8f1e206f7db6f7a225760790b
Erik Mitchell <erikm@prototype-eq.com>
2014-05-28 11:05:17 -0500

    matching projects under HES directory

--- setup-hes.el
+++ setup-hes.el
@@ -2,17 +2,15 @@
 
 (add-hook 'js2-mode-hook
 	  (lambda ()
-	    (if (string< "/home/erikm/Projects/HES" (file-name-directory (buffer-file-name)))
+	    (if (string-prefix-p "/home/erikm/Projects/HES" (file-name-directory (buffer-file-name)))
 		(progn
 		  (message "Setting indent for HES .js development")
 		  (setq-default tab-width 4)
-		  (setq indent-tabs-mode t)
-		  (smart-tabs-advice js2-indent-line js2-basic-offset))
+		  (setq indent-tabs-mode t))
 	      (message (concat "Did not match project " (file-name-directory (buffer-file-name))))
 	      )))
 
 (provide 'setup-hes)
-
 
 
 	
