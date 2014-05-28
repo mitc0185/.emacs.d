21c21e74a7849aa0b3f16b5218c71843e3ed1639
Erik Mitchell <erikm@prototype-eq.com>
2014-05-28 11:05:44 -0500

    changed font

--- custom.el
+++ custom.el
@@ -3,10 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 90 :width normal)))))
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/Orgmode/standish_midway.org" "~/Dropbox/Orgmode/techdump.org" "~/Dropbox/Orgmode/smeng.org" "~/Dropbox/Orgmode/hes.org" "~/Dropbox/Orgmode/index.org" "~/Dropbox/Orgmode/vendita.org" "~/Dropbox/Orgmode/healthpartners.org" "~/Dropbox/Orgmode/prototype_eq.org"))))
--- init.el
+++ init.el
@@ -36,15 +36,50 @@
 (load custom-file)
 
 ;; Write backup files to own directory
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; act like we've been here before
+(setq inhibit-startup-message t)
+
+;; Are we on a mac?
+(setq is-mac (equal system-type 'darwin))
+
+;; Are we on windows?
+(setq is-windows (equal system-type 'windows-nt))
+
+;; Set path to dependencies
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path site-lisp-dir)
+
+;; Settings for currently logged in user
+(setq user-settings-dir
+      (concat user-emacs-directory "users/" user-login-name))
+(add-to-list 'load-path user-settings-dir)
+
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
 (setq backup-directory-alist
       `(("." . ,(expand-file-name
                  (concat user-emacs-directory "backups")))))
+
+;; Set Font
+(set-face-attribute 'default nil :font  "Anonymous Pro-8" )
 
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
 
 ;; Setup extensions
-(eval-after-load 'org-mode '(require 'setup-org-mode))
+(eval-after-load 'org '(require 'setup-org))
 (eval-after-load 'web-mode '(require 'setup-web-mode))
 (eval-after-load 'ledger-mode '(require 'setup-ledger-mode))
 
