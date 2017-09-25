5e4733e38f5563a57ae81dac3633382add291bc6
Erik K. Mitchell <erik.k.mitchell@target.com>
2017-09-25 14:22:31 -0500

    turned off warnings on semicolon

--- lisp/setup-js2-mode.el
+++ lisp/setup-js2-mode.el
@@ -6,6 +6,8 @@
 
 (add-hook 'js2-mode-hook
 	  (lambda ()
-	    (setq indent-tabs-mode nil)))
+	    (setq indent-tabs-mode nil)
+	    (setq js2-strict-missing-semi-warning nil)
+	    ))
 
 (provide 'setup-js2-mode)
